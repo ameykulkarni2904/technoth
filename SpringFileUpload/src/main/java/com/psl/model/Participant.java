7b71e1f52e93288733b249629f3c72a1c0114f5e
Amey Kulkarni <amey_kulkarni2@persistent.com>
2016-01-19 15:42:04 +0530

    spring file upload

---
+++ SpringFileUpload/src/main/java/com/psl/model/Participant.java
@@ -0,0 +1,70 @@
+package com.psl.model;
+
+public class Participant {
+	private int employee_id;
+	private String employee_name;
+	private String mail_id;
+	private String batch;
+	private String team_name;
+	private long contact_no;
+	public int getEmployee_id() {
+		return employee_id;
+	}
+	public void setEmployee_id(int employee_id) {
+		this.employee_id = employee_id;
+	}
+	public String getEmployee_name() {
+		return employee_name;
+	}
+	public void setEmployee_name(String employee_name) {
+		this.employee_name = employee_name;
+	}
+	public String getMail_id() {
+		return mail_id;
+	}
+	public void setMail_id(String mail_id) {
+		this.mail_id = mail_id;
+	}
+	public long getContact_no() {
+		return contact_no;
+	}
+	public void setContact_no(long contact_no) {
+		this.contact_no = contact_no;
+	}
+	
+	
+	public String getBatch() {
+		return batch;
+	}
+	public void setBatch(String batch) {
+		this.batch = batch;
+	}
+	public String getTeam_name() {
+		return team_name;
+	}
+	@Override
+	public String toString() {
+		return "Participant [employee_id=" + employee_id + ", employee_name="
+				+ employee_name + ", mail_id=" + mail_id + ", batch=" + batch
+				+ ", team_name=" + team_name + ", contact_no=" + contact_no
+				+ "]";
+	}
+	public void setTeam_name(String team_name) {
+		this.team_name = team_name;
+	}
+	
+	public Participant(int employee_id, String employee_name, String mail_id,
+			String batch, String team_name, long contact_no) {
+		super();
+		this.employee_id = employee_id;
+		this.employee_name = employee_name;
+		this.mail_id = mail_id;
+		this.batch = batch;
+		this.team_name = team_name;
+		this.contact_no = contact_no;
+	}
+	public Participant() {
+		// TODO Auto-generated constructor stub
+	}
+	
+}
